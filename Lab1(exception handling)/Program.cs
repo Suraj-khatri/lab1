d7c610369a83a2d2ac91cd84d1dd49350a9d52d3
Suraj <surajpoudelkhatri@gmail.com>
2023-07-01 13:14:10 +0545

    initial commit

---
+++ Lab1(exception handling)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_exception_handling_
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int x = 10;
+                int y = 0;
+                int z = x/y;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("finally block");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
